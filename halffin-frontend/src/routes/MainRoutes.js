e8cedd9c60d86a6c69ea10c195fa9b2a056e4bd8
VisarutDearLotuss <89439879+VisarutDearLotuss@users.noreply.github.com>
2021-11-11 09:34:01 +0700

    Update MainRoutes.js

--- halffin-frontend/src/routes/MainRoutes.js
+++ halffin-frontend/src/routes/MainRoutes.js
@@ -16,8 +16,8 @@ const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons
 
 // sample page routing
 const SamplePage = Loadable(lazy(() => import('views/sample-page')));
-const MyProduct = Loadable(lazy(() => import('views/product/myProduct')));
-const CreateProduct = Loadable(lazy(() => import('views/product/createProduct')));
+const MyProduct = Loadable(lazy(() => import('views/product/MyProduct')));
+const CreateProduct = Loadable(lazy(() => import('views/product/CreateProduct')));
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
