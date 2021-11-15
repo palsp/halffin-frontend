e9d922600744446c38b91dfe1289e9baf40b60a5
palsp <palsp2406@gmail.com>
2021-11-15 22:17:51 +0700

    chore: delete unused file

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -5,35 +5,19 @@ import MainLayout from "layout/MainLayout";
 import Loadable from "ui-component/Loadable";
 import { Navigate } from "react-router-dom";
 
-// dashboard routing
-const DashboardDefault = Loadable(
-  lazy(() => import("views/dashboard/Default"))
-);
 const ShoppingHomePage = Loadable(
   lazy(() => import("views/shoppingHomePage/ShoppingHomePage"))
 );
 
-// utilities routing
-const UtilsTypography = Loadable(
-  lazy(() => import("views/utilities/Typography"))
-);
-const UtilsColor = Loadable(lazy(() => import("views/utilities/Color")));
-const UtilsShadow = Loadable(lazy(() => import("views/utilities/Shadow")));
-const UtilsMaterialIcons = Loadable(
-  lazy(() => import("views/utilities/MaterialIcons"))
-);
-const UtilsTablerIcons = Loadable(
-  lazy(() => import("views/utilities/TablerIcons"))
-);
-
 // sample page routing
-const SamplePage = Loadable(lazy(() => import("views/sample-page")));
 const MarketPlace = Loadable(lazy(() => import("views/product/MarketPlace")));
 const CreateProduct = Loadable(
   lazy(() => import("views/product/CreateProduct"))
 );
 const UserProfile = Loadable(lazy(() => import("views/user/UserProfile")));
-const SingleProduct = Loadable(lazy(() => import('views/product/SingleProduct') ));
+const SingleProduct = Loadable(
+  lazy(() => import("views/product/SingleProduct"))
+);
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
@@ -49,34 +33,6 @@ const MainRoutes = {
       element: <ShoppingHomePage />,
     },
     {
-      path: "/dashboard/default",
-      element: <DashboardDefault />,
-    },
-    {
-      path: "/utils/util-typography",
-      element: <UtilsTypography />,
-    },
-    {
-      path: "/utils/util-color",
-      element: <UtilsColor />,
-    },
-    {
-      path: "/utils/util-shadow",
-      element: <UtilsShadow />,
-    },
-    {
-      path: "/icons/tabler-icons",
-      element: <UtilsTablerIcons />,
-    },
-    {
-      path: "/icons/material-icons",
-      element: <UtilsMaterialIcons />,
-    },
-    {
-      path: "/sample-page",
-      element: <SamplePage />,
-    },
-    {
       path: "/my-product/create",
       element: <CreateProduct />,
     },
@@ -89,7 +45,7 @@ const MainRoutes = {
       element: <Navigate to="/" />,
     },
     {
-      path: '/product/id',
+      path: "/product/id",
       element: <SingleProduct />,
     },
   ],
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,10 @@ import { useRoutes } from "react-router-dom";
 
 // routes
 import MainRoutes from "./MainRoutes";
-import AuthenticationRoutes from "./AuthenticationRoutes";
 import config from "config";
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-  return useRoutes([MainRoutes, AuthenticationRoutes], config.basename);
+  return useRoutes([MainRoutes], config.basename);
 }
