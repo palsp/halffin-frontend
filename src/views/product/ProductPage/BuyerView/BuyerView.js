dc078decd59f613892a2d80877dd76c6fbd68814
Krittamook Aksornchindarat <krittamook@hotmail.com>
2021-11-19 00:06:52 +0700

    feat: buyer allow seller ACL

--- src/views/product/ProductPage/BuyerView/BuyerView.js
+++ src/views/product/ProductPage/BuyerView/BuyerView.js
@@ -1,26 +1,40 @@
 // material-ui
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 // project imports
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 // hooks
-import { useEscrow, useTransaction } from "hooks";
-import { useProduct } from "context";
-import TransactionModal from "../../../../ui-component/extended/Modal/TransactionModal";
+import { useEscrow, useTransaction } from 'hooks';
+import { useProduct } from 'context';
+import TransactionModal from '../../../../ui-component/extended/Modal/TransactionModal';
+import { useMoralis } from 'react-moralis';
 
 const BuyerView = ({ product }) => {
   const navigate = useNavigate();
   const { updateProductInfo } = useProduct();
   const { signAndSendTransaction, txState, ...txProps } = useTransaction([
-    "Select Shipping Address",
-    "Sign transaction",
-    "Transaction initiated",
-    "Confirmation",
+    'Select Shipping Address',
+    'Sign transaction',
+    'Transaction initiated',
+    'Confirmation',
   ]);
   const { handleNextStep, handleOpen } = txProps;
   const { order } = useEscrow();
+  const { Moralis } = useMoralis();
+
+  const allowPermission = async () => {
+    const seller = await Moralis.Cloud.run('getUserByEthAddress', {
+      targetEthAddr: product.owner,
+    });
+
+    const res = await Moralis.Cloud.run('allowPermissionToUserId', {
+      targetId: seller.id,
+    });
+
+    console.log('allow permission', res);
+  };
 
   const handleClick = () => {
     handleOpen();
@@ -28,19 +42,27 @@ const BuyerView = ({ product }) => {
 
   const handleEnterShippingAddress = async () => {
     // do sth with address
+
+    // check if user have address
+    /// add address first
+
     handleNextStep();
     await handleBuy();
+
+    // Send ACL to seller
   };
 
   const handleBuy = async () => {
     await signAndSendTransaction(() => order(product.address, product.price));
     await updateProductInfo(product.id);
     // navigate to my purchase
-    navigate("/user/account-profile", { state: { value: 1 } });
+    navigate('/user/account-profile', { state: { value: 1 } });
   };
 
   return (
     <>
+      <button onClick={allowPermission}>test allow</button>
+
       {product.isAbleToBuy && (
         <>
           <TransactionModal
@@ -48,17 +70,9 @@ const BuyerView = ({ product }) => {
             {...txProps}
             components={{
               0: (
-                <Grid
-                  container
-                  direction="column"
-                  justifyContent="center"
-                  alignItems="center"
-                >
+                <Grid container direction="column" justifyContent="center" alignItems="center">
                   Enter Shipping Address
-                  <Button onClick={handleEnterShippingAddress}>
-                    {" "}
-                    Continue{" "}
-                  </Button>
+                  <Button onClick={handleEnterShippingAddress}> Continue </Button>
                 </Grid>
               ),
             }}
