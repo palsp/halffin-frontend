078a95f9a7fab4cceb42b1da03ca5417ad877a50
palsp <palsp2406@gmail.com>
2021-11-22 18:10:16 +0700

    re: tab panels

--- src/views/user/UserProfile.js
+++ src/views/user/UserProfile.js
@@ -7,13 +7,27 @@ import MainCard from "ui-component/cards/MainCard";
 import { useMoralis } from "react-moralis";
 import MuiTypography from "@mui/material/Typography";
 import { useAddress, useProduct } from "context";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Navigate } from "react-router-dom";
 import ProductList from "../product/ProductList/ProductList";
 import { shortenIfAddress } from "@usedapp/core";
 import TabPanels from "ui-component/extended/TabPanels";
 import { useTheme } from "@mui/material/styles";
 import AddressDetail from "ui-component/Address/AddressDetail";
 import FormModal from "ui-component/Address/FormModal";
+
+const MY_PRODUCT_LABELS = [
+  "In Market",
+  "Waiting For Shipment",
+  "To Be Delivered",
+  "To Be Claimed",
+  "Complete",
+];
+
+const MY_PURCHASE_LABELS = [
+  "Waiting For Shipment",
+  "To Be Delivered",
+  "Complete",
+];
 
 const UserProfile = () => {
   const theme = useTheme();
@@ -63,121 +77,116 @@ const UserProfile = () => {
     setValue(newvalue);
   };
 
-  let productsOfSeller = [];
-  let productsOfBuyers = [];
+  let myProductComponents = [];
+  let myPurchaseComponents = [];
+
   if (user) {
-    productsOfSeller = getProductsOfSeller(user.attributes.ethAddress);
-    productsOfBuyers = getProductsOfBuyer(user.attributes.ethAddress);
+    const productsOfSeller = getProductsOfSeller(user.attributes.ethAddress);
+    const productsOfBuyers = getProductsOfBuyer(user.attributes.ethAddress);
+    const myProduct = [[], [], [], [], []];
+    const myPurchase = [[], [], []];
+
+    // myProduct
+    productsOfSeller.forEach((product) => {
+      myProduct[parseInt(product._stage)].push(product);
+    });
+
+    myProductComponents = MY_PRODUCT_LABELS.map((label, index) => ({
+      label: `${label} (${myProduct[index].length})`,
+      component: <ProductList products={myProduct[index]} />,
+    }));
+
+    // myPurchase
+    productsOfBuyers.forEach((product) => {
+      if (product.isWaitForShipping) {
+        myPurchase[0].push(product);
+      } else if (product.isAbleToCheckTrackingStatus) {
+        myPurchase[1].push(product);
+      } else if (product.isAbleToClaimFund || product.isEnd) {
+        myPurchase[2].push(product);
+      }
+    });
+
+    myPurchaseComponents = MY_PURCHASE_LABELS.map((label, index) => ({
+      label: `${label} (${myPurchase[index].length})`,
+      component: <ProductList products={myPurchase[index]} />,
+    }));
   }
 
   return (
     <>
-      <MainCard style={{ display: "flex", justifyContent: "center" }}>
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Avatar
-            src="https://picsum.photos/200"
-            sx={{ width: 100, height: 100 }}
-          />
-          <MuiTypography
-            variant="subtitle1"
-            gutterBottom
-            style={{ color: theme.palette.text.base }}
+      {!user ? (
+        <Navigate to="/" />
+      ) : (
+        <MainCard style={{ display: "flex", justifyContent: "center" }}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
           >
-            {isAuthenticated
-              ? shortenIfAddress(user.attributes.ethAddress)
-              : ""}
-          </MuiTypography>
-          <MuiTypography
-            variant="subtitle2"
-            style={{ color: theme.palette.text.base }}
-          >
-            {isAuthenticated ? user.attributes.createdAt.toString() : ""}
-          </MuiTypography>
-          <TabPanels
-            value={value}
-            onChange={(e, newValue) => setValue(newValue)}
-            labels={["My Product", "My Purchase", "My Address"]}
-            components={[
-              <TabPanels
-                value={myProductValue}
-                onChange={(e, newValue) => setMyProductValue(newValue)}
-                labels={[
-                  "My product",
-                  "Waiting For Shipment",
-                  "To Be Delivered",
-                  "To Be Claimed",
-                  "Complete",
-                ]}
-                components={[
-                  <ProductList
-                    products={productsOfSeller.filter(
-                      (product) => product.isAbleToBuy
-                    )}
-                  />,
-                  <ProductList
-                    products={productsOfSeller.filter(
-                      (product) => product.isWaitForShipping
-                    )}
-                  />,
-                  <ProductList
-                    products={productsOfSeller.filter(
-                      (product) => product.isAbleToCheckTrackingStatus
-                    )}
-                  />,
-                  <ProductList
-                    products={productsOfSeller.filter(
-                      (product) => product.isAbleToClaimFund
-                    )}
-                  />,
-                  <ProductList
-                    products={productsOfSeller.filter(
-                      (product) => product.isEnd
-                    )}
-                  />,
-                ]}
-              />,
-              <TabPanels
-                value={myPurchaseValue}
-                onChange={(e, newValue) => setMyPurchaseValue(newValue)}
-                labels={["Waiting For Shipment", "To Be Delivered", "Complete"]}
-                components={[
-                  <ProductList
-                    products={productsOfBuyers.filter(
-                      (product) => product.isWaitForShipping
-                    )}
-                  />,
-                  <ProductList
-                    products={productsOfBuyers.filter(
-                      (product) => product.isAbleToCheckTrackingStatus
-                    )}
-                  />,
-                  <ProductList
-                    products={productsOfBuyers.filter(
-                      (product) => product.isAbleToClaimFund || product.isEnd
-                    )}
-                  />,
-                ]}
-              />,
-              <>
-                <FormModal
-                  open={open}
-                  handleOpen={handleOpen}
-                  handleClose={handleClose}
-                  address={address}
-                  addAddress={addAddress}
-                />
+            <Avatar
+              src="https://picsum.photos/200"
+              sx={{ width: 100, height: 100 }}
+            />
+            <MuiTypography
+              variant="subtitle1"
+              gutterBottom
+              style={{ color: theme.palette.text.base }}
+            >
+              {shortenIfAddress(user.attributes.ethAddress)}
+            </MuiTypography>
+            <MuiTypography
+              variant="subtitle2"
+              style={{ color: theme.palette.text.base }}
+            >
+              {user.attributes.createdAt.toString()}
+            </MuiTypography>
+            <TabPanels
+              value={value}
+              onChange={(e, newValue) => setValue(newValue)}
+              components={[
+                {
+                  label: "My Product",
+                  component: (
+                    <TabPanels
+                      value={myProductValue}
+                      onChange={(e, newValue) => setMyProductValue(newValue)}
+                      components={myProductComponents}
+                    />
+                  ),
+                },
+                {
+                  label: "My Purchase",
+                  component: (
+                    <TabPanels
+                      value={myPurchaseValue}
+                      onChange={(e, newValue) => setMyPurchaseValue(newValue)}
+                      components={myPurchaseComponents}
+                    />
+                  ),
+                },
+                {
+                  label: "My Address",
+                  component: (
+                    <>
+                      <FormModal
+                        open={open}
+                        handleOpen={handleOpen}
+                        handleClose={handleClose}
+                        address={address}
+                        addAddress={addAddress}
+                      />
 
-                {user && <AddressDetail address={address} />}
-              </>,
-            ]}
-          />
-        </Grid>
-      </MainCard>
+                      {user && <AddressDetail address={address} />}
+                    </>
+                  ),
+                },
+              ]}
+            />
+          </Grid>
+        </MainCard>
+      )}
     </>
   );
 };
