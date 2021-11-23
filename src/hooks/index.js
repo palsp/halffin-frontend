87bcadcb1deb4c91d77d9c566d74fcc96a3e2732
Krittamook Aksornchindarat <krittamook@hotmail.com>
2021-11-23 14:27:42 +0700

    feat: reuse trackingId after rejection

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,8 @@
-import useEscrowFactory from "./useEscrowFactory";
-import useEscrow from "./useEscrow";
-import useWeb3 from "./useWeb3";
-import useTx from "./useTx";
-import useTransaction from "./useTransaction";
+import useEscrowFactory from './useEscrowFactory';
+import useEscrow from './useEscrow';
+import useWeb3 from './useWeb3';
+import useTx from './useTx';
+import useTransaction from './useTransaction';
+import useQuery from './useQuery';
 
-export { useEscrow, useEscrowFactory, useWeb3, useTx, useTransaction };
+export { useQuery, useEscrow, useEscrowFactory, useWeb3, useTx, useTransaction };
--- src/views/product/ProductPage/SellerView/UpdateTrackingPrompt/index.js
+++ src/views/product/ProductPage/SellerView/UpdateTrackingPrompt/index.js
@@ -1,71 +1,144 @@
-import { useState } from "react";
-import { Button, TextField } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import { useTransaction } from "hooks";
-import { defaultTxSteps } from "store/constant";
-import { createTracking } from "api/tracking_server";
-import Product from "model/Product";
-import TransactionModal from "ui-component/extended/Modal/TransactionModal";
-import { useTheme } from "@mui/material/styles";
+import { useState, useEffect } from 'react';
+import { Button, TextField } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { useTransaction, useQuery } from 'hooks';
+import { defaultTxSteps } from 'store/constant';
+import { createTracking } from 'api/tracking_server';
+import Product from 'model/Product';
+import TransactionModal from 'ui-component/extended/Modal/TransactionModal';
+import { useTheme } from '@mui/material/styles';
+import { useMoralis } from 'react-moralis';
 
 const useStyles = makeStyles((theme) => ({
   trackingForm: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 }));
 
 const UpdateTrackingPrompt = ({ product, onSendTransaction, onUpdate }) => {
   const classes = useStyles();
   const theme = useTheme();
-  const [trackingNo, setTrackingNo] = useState("");
+
+  const [shipment, setShipment] = useState({
+    trackingId: '',
+    trackingNo: '',
+    slug: '',
+  });
 
   const { signAndSendTransaction, txState, ...txProps } = useTransaction(
-    ["update shipping detail"].concat(defaultTxSteps)
+    ['update shipping detail'].concat(defaultTxSteps)
   );
 
   const { handleOpen, handleNextStep, handleError } = txProps;
-  const handleUpdateTracking = async (e) => {
-    e.preventDefault();
-    handleOpen();
+  const { Moralis } = useMoralis();
+  const { queryEqualTo } = useQuery();
+
+  useEffect(() => {
+    getTrackingNo();
+    console.log('product', product);
+  }, []);
+
+  const getTrackingNo = async () => {
     try {
-      const response = await createTracking(product.id, trackingNo.trim());
-      const trackingId = response.data.tracking.id;
+      const res = await queryEqualTo({
+        className: 'Shipment',
+        attr: 'contractAddress',
+        target: product.address,
+      });
+      if (res) {
+        setShipment({
+          trackingId: res.attributes.trackingId,
+          trackingNo: res.attributes.trackingNo,
+          slug: res.attributes.slug,
+        });
+      }
+    } catch (err) {
+      handleError(err);
+    }
+  };
+
+  const addShipmentDetail = ({ trackingNo, trackingId, slug }) => {
+    return Moralis.Cloud.run('addShipmentDetail', {
+      trackingId,
+      trackingNo,
+      slug,
+      contractAddress: product.address,
+    });
+  };
+
+  const updateTracking = async () => {
+    const response = await createTracking(product.id, shipment.trackingNo.trim());
+    const trackingId = response.data.tracking.id;
+    const slug = response.data.tracking.slug;
+
+    await addShipmentDetail({ trackingNo: shipment.trackingNo.trim(), trackingId, slug });
+  };
+
+  const handleConfirmTracking = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (shipment.trackingId.length === 0 && shipment.trackingNo.length > 0) {
+        await updateTracking();
+      }
+
       handleNextStep();
-      await signAndSendTransaction(() =>
-        onSendTransaction(product.address, trackingId)
-      );
+      await signAndSendTransaction(() => onSendTransaction(product.address, shipment.trackingId));
       await onUpdate(product.id);
     } catch (err) {
       handleError(err);
     }
   };
+
   return (
     <>
-      <TransactionModal {...txState} {...txProps} />
+      <TransactionModal
+        {...txState}
+        {...txProps}
+        components={{
+          0: (
+            <div
+              style={{
+                display: 'flex',
+                margin: '20px',
+                justifyContent: 'center',
+                flexDirection: 'column',
+              }}
+            >
+              <div style={{ display: 'flex', margin: '20px', justifyContent: 'center' }}>
+                <TextField
+                  fullWidth
+                  required
+                  label="Tracking Number"
+                  margin="normal"
+                  name="trackingNumber"
+                  type="text"
+                  defaultValue=""
+                  disabled={shipment.trackingId.length > 0}
+                  sx={{ ...theme.typography.customInput }}
+                  value={shipment.trackingNo}
+                  onChange={(e) =>
+                    setShipment((prevState) => ({ ...prevState, trackingNo: e.target.value }))
+                  }
+                />
+
+                <Button
+                  sx={{ padding: 'none' }}
+                  size="small"
+                  type="submit"
+                  onClick={handleConfirmTracking}
+                >
+                  Next
+                </Button>
+              </div>
+            </div>
+          ),
+        }}
+      />
       <form className={classes.trackingForm}>
-        <TextField
-          fullWidth
-          required
-          label="Tracking Number"
-          margin="normal"
-          name="trackingNumber"
-          type="text"
-          defaultValue=""
-          sx={{ ...theme.typography.customInput }}
-          value={trackingNo}
-          onChange={(e) => setTrackingNo(e.target.value)}
-        />
-        <Button
-          sx={{ padding: "none" }}
-          disabled={trackingNo.trim() === ""}
-          size="small"
-          type="submit"
-          onClick={handleUpdateTracking}
-        >
-          Update
-        </Button>
+        <Button onClick={handleOpen}>Update Tracking</Button>
       </form>
     </>
   );
