d015cf8099d6909202fb815754276d2b2e3598db
palsp <palsp2406@gmail.com>
2021-11-28 02:37:06 +0700

    fix: allow seller to change tracking number if exception occurs

--- cloud_function/index.js
+++ cloud_function/index.js
@@ -293,3 +293,48 @@ Moralis.Cloud.define(
   },
   validationRules
 );
+
+Moralis.Cloud.define(
+  'updateShipmentDetail',
+  async (request) => {
+    try {
+      const { contractAddress } = request.params;
+      const web3 = Moralis.web3ByChain('0x2a');
+      const contract = new web3.eth.Contract(abi, contractAddress);
+      const product = await contract.methods.product().call();
+      if (
+        !request.user.attributes.accounts.includes(product.owner.toLowerCase())
+      ) {
+        return {
+          success: false,
+          message: 'Unauthorized!',
+        };
+      }
+
+      const Shipment = Moralis.Object.extend('Shipment');
+      const query = new Moralis.Query(Shipment);
+      query.equalTo('contractAddress', contractAddress);
+      const shipment = await query.first({ useMasterKey: true });
+
+      await shipment.save(
+        { ...request.params, userId: request.user.id },
+        { useMasterKey: true }
+      );
+      await setReadACL({
+        className: 'Shipment',
+        attr: 'trackingId',
+        target: request.params.trackingId,
+        allowId: request.params.buyerId,
+        state: true,
+      });
+
+      return { success: true };
+    } catch (err) {
+      return { success: false, error: err.message };
+    }
+  },
+  {
+    fields: ShipmentDetail,
+  },
+  validationRules
+);
